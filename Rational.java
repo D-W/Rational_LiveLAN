e3c89ce040a5c7fd4217bae0a7c9cc2b355cfce1
Mei Huang <mhuang5@stuy.edu>
2015-11-19 01:36:25 -0500

    PHASE 2 COMPLETE

--- Rational.java
+++ Rational.java
@@ -1,6 +1,6 @@
 // Team 42 - Dylan Wright, Mei Huang
 // APCS1 pd5
-// HW33 -- Irrationality Stops Here
+// HW33 -- Do you even add, Bro? 
 // 2015-11-18
 
 public class Rational {
@@ -40,5 +40,57 @@ public class Rational {
     public void divide(Rational newRational) {
         numerator *= newRational.denominator;
         denominator *= newRational.numerator;
+    } 
+    public void add(Rational newRational) { 
+        if (this.denominator == newRational.denominator) {
+            numerator += newRational.numerator;
+            denominator = newRational.denominator; }
+    }
+    public void subtract(Rational newRational) {
+        if (this.denominator == newRational.denominator) {
+            numerator -= newRational.numerator;
+            denominator = newRational.denominator; } 
+    }
+    public int greatestcd(int a, int b) {
+	if (a == b) {return a;} 
+	int greater = Math.max(a,b);
+	int smaller = Math.min(a,b); 
+	if (greater%smaller == 0) {
+	    return smaller; 
+	}
+    else {
+        return greatestcd(smaller,greater%smaller); 
+	}
+    }
+	public double gcd() {
+	    return greatestcd(this.numerator,this.denominator);
+	}
+	public void reduce() {
+	    this.numerator /= gcd();
+	    this.denominator /= gcd();
+	}
+     public static void main (String[] args) { 
+        Rational r = new Rational(2,3); 
+        Rational s = new Rational(1,6);
+        Rational t = new Rational(2,6);
+        Rational q = new Rational(2,8);
+        System.out.println(s.floatValue()); //should ouput 0.5
+        System.out.println(t.gcd()); //should output 2
+        System.out.println(r); //should output 2/3
+        System.out.println(s); //should output 1/6
+        r.multiply(s); // 2/3 x 1/6
+        System.out.println(r); // should output 2/18 
+        System.out.println(s); // should output 1/6
+        r.divide(s); // 2/18 / 1/6  
+        System.out.println(r); //should output 12/18 
+        System.out.println(s); //should output 1/6 
+        s.add(t); // 1/6 + 2/6 
+        System.out.println(s);  // should output 3/6
+        System.out.println(t); // should output 2/6
+        s.subtract(t); // 3/6 - 2/6 
+        System.out.println(s); // should output 1/6 
+        System.out.println(t); // should output 2/6
+        q.reduce(); 
+        System.out.println(q); // should output 1/2
     }
 }
